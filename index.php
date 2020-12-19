eae23d3447cd6155ec9dbb0143e7686542dbcef5
Clodoaldo Brasilino <886522+clodbrasilino@users.noreply.github.com>
2020-12-19 11:28:42 -0300

    fixed access token retrieval

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     $returnArray = array();
     foreach($data as $line) {
         $explode = explode("=", $line);
-        $returnArray[$explode[0]] = $explode[1];
+        $returnArray[$explode[0]] = preg_replace( "/\n/", "", $explode[1] );
     }
     return $returnArray;
   }
@@ -15,18 +15,18 @@
     $username = $credentials["client_id"];
     $password = $credentials["client_secret"];
     $url = "https://us.battle.net/oauth/token";
-    $request_headers = [
+    $headers = [
       "Content-Type: application/x-www-form-urlencoded",
     ];
-    $request_data = [
-      'grant_type' => 'client_credentials'
+    $payload = [
+      'grant_type' => 'client_credentials',
     ];
     $request = curl_init();
     curl_setopt($request, CURLOPT_URL, $url);
     curl_setopt($request, CURLOPT_POST, 1);
-    curl_setopt($request, CURLOPT_HTTPHEADER, $request_headers);
+    curl_setopt($request, CURLOPT_HTTPHEADER, $header);
     curl_setopt($request, CURLOPT_USERPWD, $username . ":" . $password);
-
+    curl_setopt($request, CURLOPT_POSTFIELDS, $payload);
     $response = curl_exec($request);
     return json_decode($response, true);
   }
